638194300257a5b5d0fe3edec8fceda5bf671efd
Akinkunmi <akinkunmioye42@gmail.com>
2026-07-29 14:24:30 +0100

    Remove settings search validation

--- src/client/routes/__root.tsx
+++ src/client/routes/__root.tsx
@@ -1,10 +1,6 @@
 import { createRootRoute } from "@tanstack/react-router";
 import { RootShell } from "../components/layout/root-shell";
-import { isSystemSettingsTab, type SystemSettingsTab } from "../components/modals/system-settings-types";
 
 export const Route = createRootRoute({
-  validateSearch: (search): { settings?: SystemSettingsTab } => ({
-    settings: isSystemSettingsTab(search.settings) ? search.settings : undefined
-  }),
   component: RootShell
 });
