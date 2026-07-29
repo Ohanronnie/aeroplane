4e8032e124c42ce0a398c25234af13389dc49fb8
Akinkunmi <akinkunmioye42@gmail.com>
2026-07-29 14:31:02 +0100

    Use router link for settings shortcut

--- src/client/components/layout/global-system-settings.tsx
+++ src/client/components/layout/global-system-settings.tsx
@@ -1,6 +1,5 @@
 import { Settings01Icon } from "@hugeicons/core-free-icons";
-import { useLocation } from "@tanstack/react-router";
-import { settingsPathForTab } from "../../features/settings/settings-pages";
+import { Link, useLocation } from "@tanstack/react-router";
 import { AppIcon } from "../ui/primitives";
 
 function shouldHideSettingsButton(pathname: string) {
@@ -17,13 +16,14 @@ export function GlobalSystemSettings() {
   if (shouldHideSettingsButton(location.pathname)) return null;
 
   return (
-    <a
-      href={settingsPathForTab()}
+    <Link
+      to="/settings/$settingsPage"
+      params={{ settingsPage: "domains" }}
       className="fixed right-5 top-5 z-40 inline-flex h-10 w-10 items-center justify-center border border-white/15 bg-black text-zinc-400 transition-colors hover:border-white/35 hover:bg-zinc-900 hover:text-white"
       title="System settings"
       aria-label="System settings"
     >
       <AppIcon icon={Settings01Icon} size={16} />
-    </a>
+    </Link>
   );
 }
